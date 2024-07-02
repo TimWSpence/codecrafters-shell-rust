3c25424035fdfa258f92e2534f1a5f021d4a1d23
Tim Spence <timothywspence@gmail.com>
2024-07-02 12:35:25 +0100

    Implement type builtin

--- src/builtins.rs
+++ src/builtins.rs
@@ -4,6 +4,7 @@ pub fn dispatch(cmd: &str) -> Option<Box<dyn Handler>> {
     match cmd {
         "exit" => Some(Box::new(ExitHandler {})),
         "echo" => Some(Box::new(EchoHandler {})),
+        "type" => Some(Box::new(TypeHandler {})),
         _ => None,
     }
 }
@@ -28,5 +29,30 @@ impl Handler for EchoHandler {
     fn handle(&self, args: Vec<&str>) -> Result<()> {
         println!("{}", args.join(" "));
         Ok(())
+    }
+}
+
+struct TypeHandler {}
+
+impl Handler for TypeHandler {
+    fn handle(&self, args: Vec<&str>) -> Result<()> {
+        if args.len() == 1 {
+            let cmd = args.first().unwrap();
+            let tpe = match dispatch(cmd) {
+                Some(_) => Some("is a shell builtin"),
+                None => None,
+            };
+
+            if let Some(tpe) = tpe {
+                println!("{} {}", cmd, tpe)
+            } else {
+                println!("{}: not found", cmd)
+            };
+
+            Ok(())
+        } else {
+            eprintln!("No argument provided for `type`");
+            Ok(())
+        }
     }
 }
