d6ced3735f22c659926cffacfd57d3087697a30e
Tim Spence <timothywspence@gmail.com>
2024-07-02 11:40:17 +0100

    Make it run in a loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+use anyhow::*;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-
-use anyhow::*;
 
 fn main() -> Result<()> {
     print!("$ ");
@@ -9,12 +8,12 @@ fn main() -> Result<()> {
 
     // Wait for user input
     let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input)?;
+    loop {
+        let mut input = String::new();
+        stdin.read_line(&mut input)?;
 
-    if let Some(cmd) = input.split_whitespace().next() {
-        println!("{cmd}: command not found")
+        if let Some(cmd) = input.split_whitespace().next() {
+            println!("{cmd}: command not found")
+        }
     }
-
-    Ok(())
 }
