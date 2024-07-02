6e20f9fa3ca34c3c823a100cc54c69bd25d79a45
Tim Spence <timothywspence@gmail.com>
2024-07-02 11:43:57 +0100

    Print prompt on every loop

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use anyhow::*;
 use std::io::{self, Write};
 
 fn main() -> Result<()> {
-    print!("$ ");
-    io::stdout().flush()?;
-
     // Wait for user input
     let stdin = io::stdin();
     loop {
         let mut input = String::new();
+
+        print!("$ ");
+        io::stdout().flush()?;
+
         stdin.read_line(&mut input)?;
 
         if let Some(cmd) = input.split_whitespace().next() {
