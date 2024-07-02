aa1f81fc6900037ccdb772e19932965279fb6e74
Tim Spence <timothywspence@gmail.com>
2024-07-02 11:37:32 +0100

    Stage 2 - command not found

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+use anyhow::*;
+
+fn main() -> Result<()> {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
 
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    stdin.read_line(&mut input)?;
+
+    if let Some(cmd) = input.split_whitespace().next() {
+        println!("{cmd}: command not found")
+    }
+
+    Ok(())
 }
